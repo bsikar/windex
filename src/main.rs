de65aa7e219c3c22845efc2d2cd5d8cb7a0d7b2c
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-10-13 07:39:13 -0500

    basic working

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,57 @@
-// good resource:
-// https://tronche.com/gui/x/xlib/
-mod client;
-mod config;
-mod windex;
+use fork::{fork, Fork};
+use libc::close;
+use std::mem::MaybeUninit;
+use std::process::Command;
+use std::ptr::null;
+use x11::keysym::XK_Return;
+use x11::xlib::*;
 
-use std::sync::atomic::AtomicBool;
-use std::sync::Arc;
-use windex::Windex;
-use x11::xlib::XInitThreads;
+#[allow(non_upper_case_globals)]
+fn main() {
+    unsafe {
+        let display = XOpenDisplay(null());
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // When a child processes ends, this process will not be signaled, it will be ignored.
-    let term = Arc::new(AtomicBool::new(false));
-    signal_hook::flag::register(signal_hook::consts::SIGCHLD, Arc::clone(&term))?;
+        if display.is_null() {
+            panic!("cannot open display");
+        }
 
-    if unsafe { XInitThreads() } == 0 {
-        panic!("XInitThreads failed");
-    }
+        XGrabKey(
+            display,
+            XKeysymToKeycode(display, XK_Return as u64) as i32,
+            Mod1Mask,
+            XDefaultRootWindow(display),
+            True,
+            GrabModeAsync,
+            GrabModeAsync,
+        );
 
-    let mut wm = Windex::new();
+        loop {
+            let event = {
+                let mut event = MaybeUninit::uninit();
+                XNextEvent(display, event.as_mut_ptr());
+                &mut event.assume_init()
+            };
 
-    loop {
-        wm.handle_events();
+            if event.type_ == KeyPress {
+                match fork() {
+                    Ok(Fork::Parent(_)) => {
+                        if XInitThreads() == 0 {
+                            panic!("XInitThreads failed");
+                        }
+                    }
+                    Ok(Fork::Child) => {
+                        if !display.is_null() {
+                            // NOTE this might break shit
+                            close(XConnectionNumber(display));
+                        }
+
+                        Command::new("st")
+                            .spawn()
+                            .unwrap_or_else(|_| panic!("failed to execute st"));
+                    }
+                    Err(e) => panic!("{}", e),
+                }
+            }
+        }
     }
 }
